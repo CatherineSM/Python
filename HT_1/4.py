9bc83f855fa8f8a215b4a396df5b42ae6399b444
Kate Serhiichuk <sergihuk95k@gmail.com>
2021-11-22 11:33:04 +0200

    Add files via upload

--- HT_1/4.py
+++ HT_1/4.py
@@ -1,18 +1,8 @@
-#4. Write a script to concatenate N strings.
+#Write a script to concatenate N strings
 
-number = 3
-n1 = input()
-n2 = input()
-n3 = input()
-print(n1 + n2 + n3)
-
-#number = int(input())
-#for i in range(number):
-#    input() 
-#s = ''.join([input()])
-
-#print(a)
-
-#array = []
-#for _ in range(N):
-#    array.append(int(input()))
+number = int(input())
+result = ''
+for step in range(number):
+    user_input = input()
+    result = result + user_input
+print(result)
