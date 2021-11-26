27238f4717d8fd953916bea35b75b532aff71d66
Kate Serhiichuk <sergihuk95k@gmail.com>
2021-11-26 10:27:46 +0200

    Update 2.py

--- HT_3/2.py
+++ HT_3/2.py
@@ -6,7 +6,9 @@ end_year = int(input("Введите год окончания отсчета"))
 
 list1 = []
 for i in range(start_year, end_year + 1):
-    if i % 4 == 0:
+    if i % 400 == 0:
+        list1.append(i)
+    elif i % 4 == 0 and i % 100 != 0:
         list1.append(i)
 
 print('Высокосные года в указанном промежутке:', list1)
