c1cc13a8a7dacb35d2dba99db565847853baa5c1
Kate Serhiichuk <sergihuk95k@gmail.com>
2021-11-29 18:56:37 +0200

    Update 7.py

--- HT_4/7.py
+++ HT_4/7.py
@@ -3,10 +3,10 @@
 
 array = [3, 3, 2, 7, 8, 1, 1, 4, 1]
 
-def conter(array):
+def counter(array):
     array_d = {}.fromkeys(array, 0)
     for a in array:
         array_d[a] += 1
     return  array_d
    
-print(conter(array))
+print(counter(array))
