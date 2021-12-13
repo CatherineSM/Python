1a2fdff4f21888a56b49f4e528ee4478278c0f27
Kate Serhiichuk <sergihuk95k@gmail.com>
2021-12-13 22:14:24 +0200

    Update 2.py

--- HT_7/2/2.py
+++ HT_7/2/2.py
@@ -25,16 +25,30 @@ def blocks(user_file, user_number):
    with open(user_file, encoding ='utf-8') as file:
       file_list = list(file.read())
       file_len = len(file_list)
-      if user_number * 3 > file_len:
+      if user_number > file_len:
          raise ValueError("В файле недостаточно символов")
       elif user_number <= 0:  
          raise ValueError("Количество символов в блоке должно быть плюсовым")
+      elif file_len == 2 and user_number == 1:
+         block1 = file_list[0]
+         block3 = 'Недостаточно символов для выведения третьего блока'
+         blok2 = file_list[-1]
+      elif file_len == 2 and user_number == 1:
+         block1 = file_list[2]
+         block3 = 'Недостаточно символов для выведения третьего блока'
+         blok2 = file_list[-2]         
+      elif file_len == 1:
+         block1 = file_list
+         block3 = 'Недостаточно символов для выведения третьего блока'
+         blok2 = 'Недостаточно символов для выведения второго блока'
+      elif file_len == user_number:
+         block1 = 'Блоки идентичны', file_list        
       else:
             block1 = file_list[:user_number]
             block3 = file_list[-user_number: len(file_list)]
             center = math.ceil(len(file_list)/2)
             blok2 = file_list[center - math.ceil(user_number/2): center + math.floor(user_number/2)]  
-   return block1, blok2, block3
+   return  block1, blok2, block3
 
 print(blocks(user_file, user_number))
       
