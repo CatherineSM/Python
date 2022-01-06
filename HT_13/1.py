7a309413936be0a9f82a30c19139926ad87b0afe
Kate Serhiichuk <sergihuk95k@gmail.com>
2022-01-06 18:09:42 +0200

    Add files via upload

--- HT_13/1.py
+++ HT_13/1.py
@@ -1,4 +1,22 @@
+#1. Створити клас Calc, який буде мати атребут last_result та 4 методи. Методи повинні виконувати математичні операції 
+#з 2-ма числами, а саме додавання, віднімання, множення, ділення.
+
+#- Якщо під час створення екземпляру класу звернутися до атребута last_result він повинен повернути пусте значення
+
+#- Якщо використати один з методів - last_result повенен повернути результат виконання попереднього методу.
+
+#- Додати документування в клас (можете почитати цю статтю: https://realpython.com/documenting-python-code/ )
+
+
+
+
 class Calc:
+    """Description
+    
+    Class has four methods for simple calculation with two numbers: additions, subtraction, multiplication & dividing.
+    Result of each method saves in attribute last_result"""
+
+
 	last_result = None
 
 	def numbers_sum (self, num1, num2):
@@ -15,7 +33,7 @@ class Calc:
 			Calc.last_result = num1 - num2
 		return Calc.last_result
 
-	def numbers_product (self, num1, num2):
+	def numbers_multiple (self, num1, num2):
 		if str(num1).isdigit() == False or str(num2).isdigit() == False:
 			return print("Аргументы num1 и num2 должны быть числами")
 		else:
@@ -34,5 +52,5 @@ class Calc:
 example = Calc()
 print(example.numbers_division(7, 7))
 print(example.last_result)
-print(example.numbers_product(7, 7))
+print(example.numbers_multiple(7, 7))
 print(example.last_result)
